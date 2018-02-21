77a7ac6692712b1c8ba7c711c0048bd92717ffd9
t-uchida <t-uchida@e-reverse.com>
2018-02-22 00:08:01 +0900

    Flow

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ var button = document.querySelector('button');
 
 // Normally register event
 var count = 0;
-button.addEventListener('click', () => console.log(`Clicked ${++count} times(normally event)`));
+var rate = 1000;
+var lastClick = Date.now() - rate;
+button.addEventListener('click', () => {
+  if (Date.now() - lastClick >= rate) {
+    console.log(`Clicked ${++count} times(normally event)`);
+    lastClick = Date.now();
+  }
+});
 
 // RxJS
 Rx.Observable.fromEvent(button, 'click')
+  .throttleTime(1000)
   .scan(count => count + 1, 0)
   .subscribe(count => console.log(`Clicked ${count} times(RxJS)`));
