fc35d252a19793b32633809002d0b8418ea32264
t-uchida <t-uchida@e-reverse.com>
2018-02-22 00:12:42 +0900

    Observable

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-var button = document.querySelector('button');
-
-// Normally register event
-var count = 0;
-var rate = 1000;
-var lastClick = Date.now() - rate;
-button.addEventListener('click', (event) => {
-  if (Date.now() - lastClick >= rate) {
-    count += event.clientX;
-    console.log(count)
-    lastClick = Date.now();
-  }
-});
-
-// RxJS
-Rx.Observable.fromEvent(button, 'click')
-  .throttleTime(1000)
-  .map(event => event.clientX)
-  .scan((count, clientX) => count + clientX, 0)
-  .subscribe(count => console.log(count));
+var observable = Rx.Observable.create(function (observer) {
+    observer.next(1);
+    observer.next(2);
+    observer.next(3);
+    setTimeout(() => {
+      observer.next(4);
+      observer.complete();
+    }, 1000);
+  });
+  
+  console.log('just before subscribe');
+  observable.subscribe({
+    next: x => console.log('got value ' + x),
+    error: err => console.error('something wrong occurred: ' + err),
+    complete: () => console.log('done'),
+  });
+  console.log('just after subscribe');
+  
