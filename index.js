1fa22c38c777b56c9db237790893553b70ac1292
t-uchida <t-uchida@e-reverse.com>
2018-02-24 00:14:32 +0900

    Anatomy of an Observable

--- index.js
+++ index.js
@@ -1,87 +1,74 @@
-/** Function */
-// function foo() {
-//   console.log('Hello');
-//   return 42;
-// }
-
-// var x = foo.call(); // same as foo()
-// console.log(x);
-// var y = foo.call(); // same as foo()
-// console.log(y);
-
-/** Observable */
-// var foo = Rx.Observable.create(function (observer) {
-//   console.log('Hello');
-//   observer.next(42);
+// var observable = Rx.Observable.create(function subscribe(observer) {
+//   var id = setInterval(() => {
+//     observer.next('hi')
+//   }, 1000);
 // });
 
-// foo.subscribe(function (x) {
-//   console.log(x);
-// });
-// foo.subscribe(function (y) {
-//   console.log(y);
-// });
+// observable.subscribe(x => console.log(x));
 
-/** Function sync call */
-// function foo() {
-//   console.log('Hello');
-//   return 42;
-// }
-
-// console.log('before');
-// console.log(foo.call());
-// console.log('after');
-
-/** Observable sync call */
-// var foo = Rx.Observable.create(function (observer) {
-//   console.log('Hello');
-//   observer.next(42);
+// var observable = Rx.Observable.create(function subscribe(observer) {
+//   observer.next(1);
+//   observer.next(2);
+//   observer.next(3);
+//   observer.complete();
 // });
 
-// console.log('before');
-// foo.subscribe(function (x) {
-//   console.log(x);
-// });
-// console.log('after');
-
-/** Function can't return multiple values over time */
-// function foo() {
-//   console.log('Hello');
-//   return 42;
-//   return 100; // dead code. will never happen
-// }
-
-// console.log('before');
-// console.log(foo.call());
-// console.log('after');
-
-/** Observable can return multiple values over time */
-// var foo = Rx.Observable.create(function (observer) {
-//   console.log('Hello');
-//   observer.next(42);
-//   observer.next(100); // "return" another value
-//   observer.next(200); // "return" yet another
+// var observable = Rx.Observable.create(function subscribe(observer) {
+//   observer.next(1);
+//   observer.next(2);
+//   observer.next(3);
+//   observer.complete();
+//   observer.next(4); // Is not delivered because it would violate the contract
 // });
 
-// console.log('before');
-// foo.subscribe(function (x) {
-//   console.log(x);
+// var observable = Rx.Observable.create(function subscribe(observer) {
+//   try {
+//     observer.next(1);
+//     observer.next(2);
+//     observer.next(3);
+//     throw new Error('MyError');
+//     observer.complete();
+//   } catch (err) {
+//     observer.error(err); // delivers an error if it caught one
+//   }
 // });
-// console.log('after');
 
-/** But Observable can also return values asynchronously */
-var foo = Rx.Observable.create(function (observer) {
-  console.log('Hello');
-  observer.next(42);
-  observer.next(100);
-  observer.next(200);
-  setTimeout(() => {
-    observer.next(300); // happens asynchronously
+// observable.subscribe(
+//   x => console.log(x),
+//   err => console.error(err)
+// );
+
+// var observable = Rx.Observable.create(function subscribe(observer) {
+//   observer.next(1);
+//   observer.next(2);
+//   observer.next(3);
+//   observer.complete();
+// });
+
+// var subscription = observable.subscribe(x => console.log(x));
+
+// // Later:
+// subscription.unsubscribe();
+
+var observable = Rx.Observable.create(function subscribe(observer) {
+  // Keep track of the interval resource
+  var intervalID = setInterval(() => {
+    observer.next('hi');
   }, 1000);
+
+  // Provide a way of canceling and disposing the interval resource
+  return function unsubscribe() {
+    clearInterval(intervalID);
+  };
 });
 
-console.log('before');
-foo.subscribe(function (x) {
-  console.log(x);
-});
-console.log('after');
+var subscription = observable.subscribe(x => console.log(x));
+
+// // Later:
+// subscription.unsubscribe();
+
+setTimeout(() => {
+  // Later:
+  subscription.unsubscribe();
+}, 3000);
+
