d28a4b6f416213b65052f9be67cf2e0b500c17e5
t-uchida <t-uchida@e-reverse.com>
2018-02-24 18:42:08 +0900

    Observer

--- index.js
+++ index.js
@@ -1,74 +1,31 @@
-// var observable = Rx.Observable.create(function subscribe(observer) {
-//   var id = setInterval(() => {
-//     observer.next('hi')
-//   }, 1000);
-// });
-
-// observable.subscribe(x => console.log(x));
-
-// var observable = Rx.Observable.create(function subscribe(observer) {
-//   observer.next(1);
-//   observer.next(2);
-//   observer.next(3);
-//   observer.complete();
-// });
-
-// var observable = Rx.Observable.create(function subscribe(observer) {
-//   observer.next(1);
-//   observer.next(2);
-//   observer.next(3);
-//   observer.complete();
-//   observer.next(4); // Is not delivered because it would violate the contract
-// });
-
-// var observable = Rx.Observable.create(function subscribe(observer) {
-//   try {
-//     observer.next(1);
-//     observer.next(2);
-//     observer.next(3);
-//     throw new Error('MyError');
-//     observer.complete();
-//   } catch (err) {
-//     observer.error(err); // delivers an error if it caught one
-//   }
-// });
-
-// observable.subscribe(
-//   x => console.log(x),
-//   err => console.error(err)
-// );
-
-// var observable = Rx.Observable.create(function subscribe(observer) {
-//   observer.next(1);
-//   observer.next(2);
-//   observer.next(3);
-//   observer.complete();
-// });
-
-// var subscription = observable.subscribe(x => console.log(x));
-
-// // Later:
-// subscription.unsubscribe();
-
 var observable = Rx.Observable.create(function subscribe(observer) {
-  // Keep track of the interval resource
-  var intervalID = setInterval(() => {
-    observer.next('hi');
-  }, 1000);
-
-  // Provide a way of canceling and disposing the interval resource
-  return function unsubscribe() {
-    clearInterval(intervalID);
-  };
+  try {
+    observer.next(1);
+    observer.next(2);
+    observer.next(3);
+    observer.complete();
+  } catch (err) {
+    observer.error(err); // delivers an error if it caught one
+  }
 });
 
-var subscription = observable.subscribe(x => console.log(x));
+// var observer = {
+//   next: x => console.log('Observer got a next value: ' + x),
+//   error: err => console.error('Observer got an error: ' + err),
+//   complete: () => console.log('Observer got a complete notification'),
+// };
 
-// // Later:
-// subscription.unsubscribe();
+// var observer = {
+//   next: x => console.log('Observer got a next value: ' + x),
+//   error: err => console.error('Observer got an error: ' + err),
+// };
 
-setTimeout(() => {
-  // Later:
-  subscription.unsubscribe();
-}, 3000);
+// observable.subscribe(observer);
 
+// observable.subscribe(x => console.log('Observer got a next value: ' + x));
+
+observable.subscribe(
+  x => console.log('Observer got a next value: ' + x),
+  err => console.error('Observer got an error: ' + err),
+  () => console.log('Observer got a complete notification')
+);
