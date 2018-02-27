c173497e4912967f5960cc9b2d0a85b444a8427c
t-uchida <t-uchida@e-reverse.com>
2018-02-27 10:11:31 +0900

    質問１ AsyncSubjectでエラーが発生した場合は最後のnextが流れるか

--- index.js
+++ index.js
@@ -1,52 +1,15 @@
-/** 
- * we take the usual simple Observable that emits values 1, 2, 3 synchronously, and use the operator observeOn to specify the async scheduler to use for delivering those values.
- */
-// var observable = Rx.Observable.create(function (observer) {
-//   observer.next(1);
-//   observer.next(2);
-//   observer.next(3);
-//   observer.complete();
-// })
-// .observeOn(Rx.Scheduler.async);
+var asyncSubject = new Rx.AsyncSubject();
 
-// console.log('just before subscribe');
-// observable.subscribe({
-//   next: x => console.log('got value ' + x),
-//   error: err => console.error('something wrong occurred: ' + err),
-//   complete: () => console.log('done'),
-// });
-// console.log('just after subscribe');
+asyncSubject.subscribe(
+  value => console.log(value),
+  err => console.log(err)
+);
 
-var observable = Rx.Observable.create(function (proxyObserver) {
-  proxyObserver.next(1);
-  proxyObserver.next(2);
-  proxyObserver.next(3);
-  proxyObserver.complete();
-})
-.observeOn(Rx.Scheduler.async);
+asyncSubject.next(1);
+asyncSubject.next(2);
+asyncSubject.next(3);
+asyncSubject.next(4);
 
-var finalObserver = {
-  next: x => console.log('got value ' + x),
-  error: err => console.error('something wrong occurred: ' + err),
-  complete: () => console.log('done'),
-};
-
-console.log('just before subscribe');
-observable.subscribe(finalObserver);
-console.log('just after subscribe');
-
-/**
- * The proxyObserver is created in observeOn(Rx.Scheduler.async), and its next(val) function is approximately the following:
-
- var proxyObserver = {
-  next: (val) => {
-    Rx.Scheduler.async.schedule(
-      (x) => finalObserver.next(x),
-      0, // delay,
-      val // will be the x for the function above
-    );
-  },
-
-  // ...
-}
- */
+// エラーが発生した場合は最後のnextには値が流れない
+asyncSubject.next(5);
+asyncSubject.error('error');
